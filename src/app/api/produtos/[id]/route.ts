317fe938775e747b2697d1f93de3f83a2ab7e5e1
AndersonGL <98903039+AndersonGL@users.noreply.github.com>
2025-12-07 14:38:34 -0300

    atual buil

--- src/app/api/produtos/[id]/route.ts
+++ src/app/api/produtos/[id]/route.ts
@@ -5,7 +5,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET(_req: Request, { params }: { params: Promise<{ id: string }> }) {
+export async function GET(_req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
 
   const produto = await prisma.produto.findUnique({
--- src/app/api/produtos/route.ts
+++ src/app/api/produtos/route.ts
@@ -1,14 +1,16 @@
 
 import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
 const prisma = new PrismaClient();
 
 export async function GET() {
   const produtos = await prisma.produto.findMany();
-  return Response.json(produtos);
+  return NextResponse.json(produtos);
 }
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   const data = await req.json();
   const novo = await prisma.produto.create({ data });
-  return Response.json(novo);
+  return NextResponse.json(novo);
 }
