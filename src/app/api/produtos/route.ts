fc157dc55d395e5b81010ae1c59609750b4963c9
AndersonGL <98903039+AndersonGL@users.noreply.github.com>
2025-12-07 15:02:22 -0300

    update

--- src/app/api/produtos/route.ts
+++ src/app/api/produtos/route.ts
@@ -1,8 +1,6 @@
 
-import { PrismaClient } from "@prisma/client";
+import prisma from "../../../lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
-
-const prisma = new PrismaClient();
 
 export async function GET() {
   const produtos = await prisma.produto.findMany();
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -11,3 +11,5 @@ const prisma = globalThis.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = prisma;
 }
+
+export default prisma;
